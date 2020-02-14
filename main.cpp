f9343075c1b04478ba270bc0ac9acdbc034dd3e5
CristianMeoño <cristianfrancisco85@gmail.com>
2020-02-13 21:30:43 -0600

    Avances: Se implementaron funciones de Deshacer y Rehacer Se implemento la funcion de lecturas de archivos Se corrigieron bugs de la funcion de busquedad y reemplazo

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <clocale>
 #include <fstream>
 #include <string>
+#include "Change.h"
 
 
 using namespace std;
@@ -17,7 +18,7 @@ void showMenu(WINDOW * win);
 void clearWin(WINDOW * win);
 
 // CREA UN NUEVO ARCHIVO
-void newArchive(WINDOW * win);
+void newArchive(WINDOW * win,string Archivo);
 
 //MUEVE HACIA ATRAS EL CURSOR
 void moveBack(WINDOW * win,int posy,int posx);
@@ -26,32 +27,34 @@ void moveBack(WINDOW * win,int posy,int posx);
 void moveFront(WINDOW * win,int posy,int posx);
 
 //ELIMINA UN CARCTER HACIA ATRAS EN PANTALLA Y LISTA
-void deleteChar(WINDOW * win,int posy,int posx,DoubleLinkedList<char> *Lista);
+void deleteChar(WINDOW * win, int posy, int posx, DoubleLinkedCircularList<char> *Lista);
 
 //DEVUELVE EL TEXTO EN LA LISTA
-string getText(DoubleLinkedList<char> *Lista);
+string getText(DoubleLinkedCircularList<char> *Lista);
 
 //GUARDA EL ARCHIVO
-void saveArchive(DoubleLinkedList<char> *Lista);
+void saveArchive(DoubleLinkedCircularList<char> *Lista, string NombreArchivo, bool Nuevo);
 
 //MENU DE REPORTES
-void showReportsMenu(WINDOW * win, DoubleLinkedList<char> *Lista, int posy, int posx);
+void showReportsMenu(WINDOW * win, DoubleLinkedCircularList<char> *Lista, int posy, int posx);
 
 // GRAFICA LA LISTA
-void graphList (DoubleLinkedList<char> *Lista);
+void graphList (DoubleLinkedCircularList<char> *Lista);
 
 //DIVIDE LA LISTA DE CARACTERES EN UNA LISTA DE PALABRAS
-void divideWords(DoubleLinkedList<char> *ListaCh , DoubleLinkedList<Word> *ListaWor);
+void divideWords(DoubleLinkedCircularList<char> *ListaCh , DoubleLinkedCircularList<Word> *ListaWor);
 
 // BUSCA Y REEMPLAZA EN BASE A PALABRAS ACTUALIZA LISTA DE CARACTERES
-int searchAndReplace (DoubleLinkedList<char> *ListaCh , DoubleLinkedList<Word> *ListaWor, string palabraBuscar , string palabraReemplazar);
+int searchAndReplace (DoubleLinkedCircularList<char> *ListaCh , DoubleLinkedCircularList<Word> *ListaWor, Stack<Change> *PilaCa , Stack<Change> *PilaCaR, string palabraBuscar , string palabraReemplazar, bool Tipo);
 
 //PIDE PARAMETROS AL USUARIO Y LOS MUESTRA EN PANTALLA
-void showSearchAndReplace(WINDOW * win, DoubleLinkedList<char> *ListaCh);
+void showSearchAndReplace(WINDOW * win, DoubleLinkedCircularList<char> *ListaCh, DoubleLinkedCircularList<Word> *ListaWor, Stack<Change> *PilaCa);
 
 // VARIABLES GLOBALES
 
 int archivo=0;
+
+// FUNCIONES PRINCIPALES
 
 int main() {
     int height,width,startx,starty;
@@ -91,12 +94,27 @@ int main() {
         switch(ch){
                 //OPCION 1 -- Crear Archivo
             case 49:
-                newArchive(win);
+                newArchive(win,"");
                 break;
                 //OPCION 2  -- Abrir Archvo
-            case 50:
-                wprintw(win,"Ctrl+Y");
+            case 50: {
+                mvwprintw(win, 18, 6, "Escriba Ruta de Archivo: ");
                 wrefresh(win);
+                bool option2Control = true;
+                string Ruta="";
+                while (option2Control) {
+                    ch=getch();
+                    if (ch > 32 && ch <= 126) {
+                        Ruta.push_back(ch);
+                        wprintw(win,"%c", ch);
+                        wrefresh(win);
+                    }
+                    if (ch == 10) {
+                        option2Control = false;
+                        newArchive(win,Ruta);
+                    }
+                }
+            }
                 break;
                 //OPCION 3 -- Archivos Recientes
             case 51:
@@ -133,9 +151,21 @@ void showMenu(WINDOW * win){
 
 }
 
-void newArchive(WINDOW * win){
+void newArchive(WINDOW * win, string Archivo){
 
-    DoubleLinkedList<char> TempArchivo;
+    //ESTRUCTURAS DE DATOS
+
+    DoubleLinkedCircularList<char> TempArchivo;
+    Stack<Change> ChangesList;
+    Stack<Change> RevertList;
+    DoubleLinkedCircularList<Word> *ListaWor = new DoubleLinkedCircularList<Word>;
+
+    //UBICACION DE CURSOR
+    int posx = 0;
+    int posy = 0;
+    int pointerList=0;
+    int ch;
+    bool Control = true;
 
     //ENCABEZADO
     attron(A_REVERSE);
@@ -145,12 +175,29 @@ void newArchive(WINDOW * win){
 
     clearWin(win);
     wmove(win,0,0);
-    //UBICACION DE CURSOR
-    int posx = 0;
-    int posy = 0;
-    int pointerList=0;
-    int ch;
-    bool Control = true;
+
+    if(Archivo!=""){
+        char letra;
+        ifstream fichero;
+        fichero.open(Archivo);
+        if(!fichero.fail()){
+            fichero.get(letra);
+            while(!fichero.eof()){
+                if(letra>=32 && letra<=126){
+                    TempArchivo.addEnd((char)letra);
+                    wprintw(win,"%c",letra);
+                    wrefresh(win);
+                    getyx(win,posy,posx);
+                }
+                fichero.get(letra);
+            }
+            fichero.close();
+        }
+        else{
+            wprintw(win,"NO SIRVE");
+        }
+    }
+
 
     while(Control){
         ch = getch();
@@ -158,17 +205,46 @@ void newArchive(WINDOW * win){
         switch (ch){
             //BUSCAR Y REEMPLAZAR ^W
             case 23:
-                showSearchAndReplace(win,&TempArchivo);
+                showSearchAndReplace(win,&TempArchivo,ListaWor,&ChangesList);
+                pointerList=TempArchivo.getSize();
+                getyx(win,posy,posx);
                 break;
             // REPORTES ^C
             case 3:
                 showReportsMenu(win, &TempArchivo, posy, posx);
                 break;
             // GUARDAR ^S
-            case 19:
-                saveArchive(&TempArchivo);
+            case 19: {
+                attron(A_REVERSE);
+                if(Archivo == ""){
+                    move(0, 2);
+                    printw("                                                                             ");
+                    move(0, 2);
+                    printw("         Nombre de Archivo (sin extension) : ");
+                    bool boolArchivo = true;
+                    while (boolArchivo) {
+                        ch = getch();
+                        if (ch > 32 && ch <= 126) {
+                            Archivo.push_back(ch);
+                            printw("%c", ch);
+                            wrefresh(win);
+                        }
+                        if (ch == 10) {
+                            boolArchivo = false;
+                            saveArchive(&TempArchivo, Archivo,true);
+                        }
+                    }
+                }
+                else{
+                    saveArchive(&TempArchivo, Archivo,false);
+                }
+                move(0,2);
+                printw("             ^W(Buscar y Reemplazar)  ^C(Reportes)  ^S(GUARDAR)             ");
+                attroff(A_REVERSE);
+                wmove(win,posy,posx);
+            }
                 break;
-            // GUARDAR ^S
+            // GUARDAR ^X
             case 24:
                 clearWin(win);
                 mvwprintw(win,0,0,"Hasta la proxima -- Presione una tecla para continuar...");
@@ -208,6 +284,39 @@ void newArchive(WINDOW * win){
                     getyx(win,posy,posx);
                 }
                 break;
+            // Ctrl+Y
+            case 25:
+                if(RevertList.getSize()>0){
+                    if(RevertList.pick().getPalabraReemplazar()!=""){
+                        string arg1=RevertList.pick().getPalabraBuscar();
+                        string arg2=RevertList.pick().getPalabraReemplazar();
+                        searchAndReplace(&TempArchivo,ListaWor,&ChangesList,&RevertList,arg1,arg2,false);
+                    }
+                    else{
+
+                    }
+                }
+                clearWin(win);
+                mvwprintw(win,0,0,getText(&TempArchivo).c_str());
+                wrefresh(win);
+                break;
+            // Ctrl+Z
+            case 26:
+
+                if(ChangesList.getSize()>0){
+                    if(ChangesList.pick().getPalabraReemplazar()!=""){
+                        string arg1=ChangesList.pick().getPalabraBuscar();
+                        string arg2=ChangesList.pick().getPalabraReemplazar();
+                        searchAndReplace(&TempArchivo,ListaWor,&ChangesList,&RevertList,arg2,arg1,true);
+                    }
+                    else{
+
+                    }
+                    clearWin(win);
+                    mvwprintw(win,0,0,getText(&TempArchivo).c_str());
+                    wrefresh(win);
+                }
+                break;
             //GUARDA UN CARACTER Y LO IMPRIME
             default:
                 if(ch>=32 && ch<=126){
@@ -239,7 +348,7 @@ void clearWin(WINDOW * win){
     wrefresh(win);
 }
 
-string getText(DoubleLinkedList<char> *Lista){
+string getText(DoubleLinkedCircularList<char> *Lista){
     string TempText="";
     for(int i =0;i<Lista->getSize();i++){
         TempText = TempText + Lista->getXNode(i);
@@ -247,17 +356,22 @@ string getText(DoubleLinkedList<char> *Lista){
     return TempText;
 }
 
-void saveArchive(DoubleLinkedList<char> *Lista){
+void saveArchive(DoubleLinkedCircularList<char> *Lista, string NombreArchivo, bool Nuevo){
 
     ofstream file;
-    file.open("./salida"+to_string(archivo)+".txt",  fstream::in | fstream::out | fstream::trunc);
+    if(Nuevo){
+        file.open("./"+NombreArchivo+".txt",  fstream::in | fstream::out | fstream::trunc);
+    }
+    else{
+        file.open(NombreArchivo,  fstream::in | fstream::out | fstream::trunc);
+    }
     archivo++;
     file << getText(Lista);
     file.close();
 
 }
 
-void graphList (DoubleLinkedList<char> *Lista){
+void graphList (DoubleLinkedCircularList<char> *Lista){
     string command = "";
     ofstream file;
     file.open("./grafica"+to_string(archivo)+".dot",  fstream::in | fstream::out | fstream::trunc);
@@ -277,7 +391,7 @@ void graphList (DoubleLinkedList<char> *Lista){
     archivo++;
 }
 
-void showReportsMenu(WINDOW * win, DoubleLinkedList<char> *Lista, int posy, int posx){
+void showReportsMenu(WINDOW * win, DoubleLinkedCircularList<char> *Lista, int posy, int posx){
     //ENCABEZADO
     attron(A_REVERSE);
     move(0,2);
@@ -316,12 +430,8 @@ void showReportsMenu(WINDOW * win, DoubleLinkedList<char> *Lista, int posy, int 
     wmove(win,posy,posx+1);
 }
 
-void showSearchAndReplace(WINDOW * win, DoubleLinkedList<char> *ListaCh ){
-
-    //SE DIVIDE EN PALABRAS LOS CARACTERES ACTUALES
-    DoubleLinkedList<Word> *ListaWor = new DoubleLinkedList<Word>;
+void showSearchAndReplace(WINDOW * win, DoubleLinkedCircularList<char> *ListaCh, DoubleLinkedCircularList<Word> *ListaWor, Stack<Change> *PilaCa){
     divideWords(ListaCh,ListaWor);
-
     //ENCABEZADO
     attron(A_REVERSE);
     move(0,2);
@@ -342,7 +452,7 @@ void showSearchAndReplace(WINDOW * win, DoubleLinkedList<char> *ListaCh ){
                 attroff(A_REVERSE);
                 string arg1 = params.substr(0, params.find(";"));
                 string arg2 = params.substr(params.find(";") + 1, params.length() - 1);
-                changes=searchAndReplace(ListaCh, ListaWor, arg1, arg2);
+                changes=searchAndReplace(ListaCh, ListaWor,PilaCa,NULL,arg1,arg2,true);
                 Control = false;
                 break;
             }
@@ -385,7 +495,7 @@ void showSearchAndReplace(WINDOW * win, DoubleLinkedList<char> *ListaCh ){
 
 //MANEJO DE DATOS
 
-void divideWords(DoubleLinkedList<char> *ListaCh , DoubleLinkedList<Word> *ListaWor){
+void divideWords(DoubleLinkedCircularList<char> *ListaCh , DoubleLinkedCircularList<Word> *ListaWor){
 
     Word *TempWord = new Word();
     string TempText="";
@@ -431,13 +541,16 @@ void divideWords(DoubleLinkedList<char> *ListaCh , DoubleLinkedList<Word> *Lista
 
 }
 
-int searchAndReplace (DoubleLinkedList<char> *ListaCh , DoubleLinkedList<Word> *ListaWor, string palabraBuscar , string palabraReemplazar){
+int searchAndReplace (DoubleLinkedCircularList<char> *ListaCh , DoubleLinkedCircularList<Word> *ListaWor, Stack<Change> *PilaCa, Stack<Change> *PilaCaR , string palabraBuscar , string palabraReemplazar, bool Tipo){
+    divideWords(ListaCh,ListaWor);
     int ListaWordSize = ListaWor->getSize();
-    int ListaChSize = ListaCh->getSize();
     int Changes=0;
+    Change TempChange;
+
     for(int i =0;i<ListaWordSize;i++){
 
         if(ListaWor->getXNode(i).getWord() == palabraBuscar){
+
             int BeginWord=ListaWor->getXNode(i).getPosBegin();
             int EndWord=ListaWor->getXNode(i).getPosEnd();
             for(int j = BeginWord ; j<=EndWord ; j++){
@@ -448,12 +561,33 @@ int searchAndReplace (DoubleLinkedList<char> *ListaCh , DoubleLinkedList<Word> *
             }
             Changes++;
         }
-        //SE ACTULIZA LISTA DE PALABRAS
-        delete ListaWor;
-        ListaWor = new DoubleLinkedList<Word>;
+        //SE ACTUALIZA LISTA DE PALABRAS
+        //delete ListaWor;
+        ListaWor = new DoubleLinkedCircularList<Word>;
         divideWords(ListaCh,ListaWor);
         ListaWordSize = ListaWor->getSize();
     }
+
+    if(Changes>0){
+        if(PilaCaR==NULL){
+            TempChange.setPalabraBuscar(palabraBuscar);
+            TempChange.setpalabraReemplazar(palabraReemplazar);
+            PilaCa->push(TempChange);
+        }
+        else {
+            //REVERTIR CAMBIO Ctrl+Z
+            if (Tipo) {
+                TempChange=PilaCa->pop();
+                PilaCaR->push(TempChange);
+            }
+                //REHACER CAMBIO Ctrl+Y
+            else {
+                TempChange=PilaCaR->pop();
+                PilaCa->push(TempChange);
+            }
+        }
+    }
+
 
     return Changes;
 
@@ -484,7 +618,7 @@ void moveFront(WINDOW * win,int posy,int posx){
 
 }
 
-void deleteChar(WINDOW * win,int posy,int posx,DoubleLinkedList<char> *Lista){
+void deleteChar(WINDOW * win, int posy, int posx, DoubleLinkedCircularList<char> *Lista){
 
     if((posx+(posy*80))<=Lista->getSize() && Lista->getSize()>0){
         if(posy>0){
